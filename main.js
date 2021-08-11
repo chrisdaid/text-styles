e19b9d639d6b5ce9e82c8d39ef98809533d38075
chrisdaid <c.chrisdb@gmail.com>
2021-08-11 09:30:00 -0400

    converted to arrow functions

--- main.js
+++ main.js
@@ -1,31 +1,18 @@
 let normalText = document.querySelector(".normal");
 
 // bold button
-var boldTog = function boldToggle() {
-    normalText.classList.toggle('bold');
-}
-
 let boldButton = document.querySelector(".boldButton");
-boldButton.addEventListener('click', boldTog);
+boldButton.addEventListener('click', () => normalText.classList.toggle('bold'));
+
 // italic button
-var italicTog = function italicToggle() {
-    normalText.classList.toggle('italic');
-}
-
 let italicButton = document.querySelector(".italicButton");
-italicButton.addEventListener('click', italicTog);
+italicButton.addEventListener('click', () => normalText.classList.toggle('italic'));
+
 // underline button
-var underlineTog = function underlineToggle() {
-    normalText.classList.toggle('under-line');
-}
-
 let underLineButton = document.querySelector(".underLineButton")
-underLineButton.addEventListener('click', underlineTog);
-// reset button
-var resetTog = function resetToggle() {
-    normalText.classList.remove('bold', 'italic', 'under-line')
-}
+underLineButton.addEventListener('click', () => normalText.classList.toggle('underline'));
 
+// reset button
 let resetButton = document.querySelector(".reset");
-resetButton.addEventListener('click', resetTog)
+resetButton.addEventListener('click', () => normalText.classList.remove('bold', 'italic', 'under-line'))
 
